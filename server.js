db083752bc03342cc2a1ac63388081d6d12fdf50
テラリウム <terrarium@MacBook.local>
2016-11-23 22:54:23 +0900

    コネクション都度生成

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ var mysql = require('mysql');
 var crypto = require('crypto');
 var squel = require('squel');
 
-var connection = mysql.createConnection({
-	host:'localhost',
-	user:'root',
-	database:'ochain'
-});
-connection.connect(function(err){
-	if(err){
-		console.error(err.stack);
-	}
-});
-
 http.createServer(function(req,res){
 	var id = 100000000 + Math.floor( Math.random() * 900000000 );
 	var sql = squel
@@ -24,15 +13,27 @@ http.createServer(function(req,res){
 	.set("create_time",squel.str("now()"))
 	.toString();
 
-	connection.query(sql, function (err, results, fields) {
+	var connection = mysql.createConnection({
+		host:'localhost',
+		user:'root',
+		database:'ochain'
+	});
+	connection.connect(function(err){
 		if(err){
 			console.error(err.stack);
 			res.writeHead(500,{"Content-Type":"text/html"});
 			res.end(sql);
 		}else{
-			res.writeHead(200,{"Content-Type":"text/html"});
-			res.end(sql);
+			connection.query(sql, function (err, results, fields) {
+				if(err){
+					console.error(err.stack);
+					res.writeHead(500,{"Content-Type":"text/html"});
+					res.end(sql);
+				}else{
+					res.writeHead(200,{"Content-Type":"text/html"});
+					res.end(sql);
+				}
+			});
 		}
 	});
-
 }).listen(1337,"160.16.213.168");
