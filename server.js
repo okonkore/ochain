b370395707e9a80eaf51921accf607284efad411
テラリウム <terrarium@MacBook.local>
2016-11-25 00:26:35 +0900

    武器のインサート

--- server.js
+++ server.js
@@ -5,14 +5,25 @@ var db = require('./db');
 
 http.createServer(function(req,res){
 	var id = 1000000000000000000 + Math.floor( Math.random() * 9000000000000000000 );
-	var sql = squel
-	.insert()
-	.into("user")
-	.set("id", id)
-	.set("name", id)
-	.set("create_time",squel.str("now()"))
-	.toString();
-	db.query(sql).then(function(result){
+	Promise.resolve().then(function(){
+		var sql = squel
+		.insert()
+		.into("user")
+		.set("id", id)
+		.set("name", id)
+		.set("create_time",squel.str("now()"))
+		.toString();
+		return db.query(sql);
+	}).then(function(){
+		var sql = squel
+		.insert()
+		.into("sheild")
+		.set("user_id", id)
+		.set("sheild_id", 1)
+		.set("create_time",squel.str("now()"))
+		.toString();
+		return db.query(sql);
+	}).then(function(result){
 		res.writeHead(200,{"Content-Type":"text/html"});
 		res.end(sql);
 	}).catch(function(err){
